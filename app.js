aa12ce1e53c0b93e843cc234874a53237022f63a
Shashank <shashank324002@gmail.com>
2020-07-12 01:10:33 +0530

    only / route

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+var items = [];
+
+app.get("/", function(req, res){
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is running on port 3000");
+});
